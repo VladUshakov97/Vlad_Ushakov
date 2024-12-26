2e459fb879b3634a1e50e411fc2c824ab562d636
VladUshakov97 <vladushakov89@gmail.com>
2024-12-26 20:14:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
-import ptbot
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 from pytimeparse import parse
+import ptbot
 
 load_dotenv('tg.env')
 
@@ -14,15 +14,11 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
     percent = "{0:.1f}".format(100 * (iteration / float(total)))
     filled_length = int(length * iteration // total)
     pbar = fill * filled_length + zfill * (length - filled_length)
-    return f'{prefix} |{pbar}| {percent}% {suffix}'
+    return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
 
 def reply(chat_id, text):
     delay = parse(text)
-    if delay is None:
-        bot.send_message(chat_id, "Неверный формат времени. Пожалуйста, используйте формат, например, '5m' или '1h 30m'.")
-        return
-
-    msg_id = bot.send_message(chat_id, "Запускаю таймер.")
+    msg_id = bot.send_message(chat_id, f"Запускаю таймер.")
     bot.create_countdown(delay, notify, chat_id=chat_id, msg_id=msg_id, delay=delay)
     bot.create_timer(delay, time_up, chat_id=chat_id, msg_id=msg_id)
 
