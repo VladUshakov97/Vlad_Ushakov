bfddf58ec74cbec46e5730aa95a1de82728f058b
VladUshakov97 <vladushakov89@gmail.com>
2024-12-26 20:16:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,36 +3,44 @@ from dotenv import load_dotenv
 from pytimeparse import parse
 import ptbot
 
-load_dotenv('tg.env')
-
+TG_ENV_FILE = 'tg.env'
 TG_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TG_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+load_dotenv(TG_ENV_FILE)
+
 bot = ptbot.Bot(TG_TOKEN)
+
 
 def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
     iteration = min(total, iteration)
     percent = "{0:.1f}".format(100 * (iteration / float(total)))
     filled_length = int(length * iteration // total)
-    pbar = fill * filled_length + zfill * (length - filled_length)
-    return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
+    progress_bar = fill * filled_length + zfill * (length - filled_length)
+    return f'{prefix} |{progress_bar}| {percent}% {suffix}'
+
 
 def reply(chat_id, text):
     delay = parse(text)
-    msg_id = bot.send_message(chat_id, f"Запускаю таймер.")
-    bot.create_countdown(delay, notify, chat_id=chat_id, msg_id=msg_id, delay=delay)
-    bot.create_timer(delay, time_up, chat_id=chat_id, msg_id=msg_id)
+    message_id = bot.send_message(chat_id, "Запускаю таймер.")
+    bot.create_countdown(delay, notify, chat_id=chat_id, message_id=message_id, delay=delay)
+    bot.create_timer(delay, time_up, chat_id=chat_id, message_id=message_id)
 
-def notify(secs_left, chat_id, msg_id, delay):
-    progress = render_progressbar(delay, delay - secs_left)
-    message = f"Осталось {secs_left} секунд.\n{progress}"
-    bot.update_message(chat_id, msg_id, message)
 
-def time_up(chat_id, msg_id):
-    bot.update_message(chat_id, msg_id, "Время пришло!")
+def notify(seconds_left, chat_id, message_id, delay):
+    progress = render_progressbar(delay, delay - seconds_left)
+    message = f"Осталось {seconds_left} секунд.\n{progress}"
+    bot.update_message(chat_id, message_id, message)
+
+
+def time_up(chat_id, message_id):
+    bot.update_message(chat_id, message_id, "Время пришло!")
+
 
 def main():
     bot.reply_on_message(reply)
     bot.run_bot()
+
 
 if __name__ == '__main__':
     main()
