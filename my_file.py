973c79db806e7db3d9aa7ee56bf87c30899086b5
VladUshakov97 <vladushakov89@gmail.com>
2025-01-12 16:14:40 +0300

    Update my_file.py

--- my_file.py
+++ my_file.py
@@ -22,45 +22,48 @@ def fetch_coordinates(apikey, address):
     lon, lat = map(float, most_relevant['GeoObject']['Point']['pos'].split(" "))
     return lat, lon
 
-apikey = '363e9cb5-e604-4cc8-929e-a7cfc10e5ac4'
+def main():
+    apikey = '363e9cb5-e604-4cc8-929e-a7cfc10e5ac4'
 
-with open('coffee.json', 'r', encoding='CP1251') as my_file:
-    coffee_data = json.load(my_file)
+    with open('coffee.json', 'r', encoding='CP1251') as my_file:
+        coffee_data = json.load(my_file)
 
-coffee_list = []
-for coffee in coffee_data:
-    if all(key in coffee for key in ['Name', 'Longitude_WGS84', 'Latitude_WGS84']):
-        coffee_list.append({
-            'Name': coffee['Name'],
-            'Coordinates': (float(coffee['Latitude_WGS84']), float(coffee['Longitude_WGS84']))
-        })
+    coffee_list = []
+    for coffee in coffee_data:
+        if all(key in coffee for key in ['Name', 'Longitude_WGS84', 'Latitude_WGS84']):
+            coffee_list.append({
+                'Name': coffee['Name'],
+                'Coordinates': (float(coffee['Latitude_WGS84']), float(coffee['Longitude_WGS84']))
+            })
 
-user_address = input('Где вы находитесь?: ')
-user_coords = fetch_coordinates(apikey, user_address)
+    user_address = input('Где вы находитесь?: ')
+    user_coords = fetch_coordinates(apikey, user_address)
 
-for coffee in coffee_list:
-    coffee['Distance'] = distance.distance(user_coords, coffee['Coordinates']).km
+    for coffee in coffee_list:
+        coffee['Distance'] = distance.distance(user_coords, coffee['Coordinates']).km
 
-sorted_coffee_list = sorted(coffee_list, key=lambda x: x['Distance'])
+    sorted_coffee_list = sorted(coffee_list, key=lambda x: x['Distance'])
 
-top_5_coffee = sorted_coffee_list[:5]
+    top_5_coffee = sorted_coffee_list[:5]
 
-pprint(top_5_coffee)
+    pprint(top_5_coffee)
 
-coffee_map = folium.Map(location=user_coords, zoom_start=14)
+    coffee_map = folium.Map(location=user_coords, zoom_start=14)
 
-folium.Marker(
-    location=user_coords,
-    popup="Вы здесь",
-    icon=folium.Icon(color="blue", icon="info-sign")
-).add_to(coffee_map)
-
-for coffee in top_5_coffee:
     folium.Marker(
-        location=coffee['Coordinates'],
-        popup=f"{coffee['Name']} ({coffee['Distance']:.2f} км)",
-        icon=folium.Icon(color="red", icon="coffee")
+        location=user_coords,
+        popup="Вы здесь",
+        icon=folium.Icon(color="blue", icon="info-sign")
     ).add_to(coffee_map)
 
-coffee_map.save("coffee_map.html")
+    for coffee in top_5_coffee:
+        folium.Marker(
+            location=coffee['Coordinates'],
+            popup=f"{coffee['Name']} ({coffee['Distance']:.2f} км)",
+            icon=folium.Icon(color="red", icon="coffee")
+        ).add_to(coffee_map)
 
+    coffee_map.save("coffee_map.html")
+
+if __name__ == "__main__":
+    main()
