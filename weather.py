b6f7afe019d434f47f98c18dcb82dddd63c601cc
VladUshakov97 <vladushakov89@gmail.com>
2025-02-13 15:14:24 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -2,18 +2,16 @@ import requests
 
 
 
-PAYLOAD = {"nTqMm": ""}
-url_template = 'https://ru.wttr.in/{}'
+payload = {"nTqMm": "", "lang=ru": ""}
+url_template = 'https://wttr.in/{}'
 cities = ['Лондон', 'Череповец', 'Шереметьево']
 
 def main():
-	for city in cities:
-		url = url_template.format(city)
-		response = requests.get(url, params=PAYLOAD)
-		response.raise_for_status()
+    for city in cities:
+        url = url_template.format(city)
+        response = requests.get(url, params=payload)
+        response.raise_for_status()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
-
-		
+    
